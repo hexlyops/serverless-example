2cc1f1fd05d0124c41bf117f66687261cae12300
Seth Back <sethjback@gmail.com>
2018-05-04 13:36:28 -0600

    v1

---
+++ serverless/handler.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  let name = 'stranger';
+  if (event.queryStringParameters && event.queryStringParameters.name) {
+    name = event.queryStringParameters.name;
+  }
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Hello, ${name}!',
+    }),
+  };
+
+  callback(null, response);
+};
